11057b9e80b1913449aecd93c07497134622c3a9
sajidsan <sajid@jut.io>
2015-11-15 22:02:52 -0800

    updated with sass and php compiling script

---
+++ index.php
@@ -0,0 +1,24 @@
+  <!DOCTYPE html>
+<html lang="en">
+  <?php include 'php/head.php'; ?>
+
+  <body>
+    <!-- navbar -->
+    <!-- navbar -->
+    <?php include 'php/navbar.php'; ?>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron home hidden-xs">
+      <div class="container">
+        <h1 class="lead wow fadeIn"style="color:white; position:absolute; top:20%">Hi I'm Sajid and<br>I design things for humans.<br>Presently mostly apps and websites.
+      </div>
+    </div>
+    <div class="black">
+      <!-- 3 x 2 grid of projects -->
+      <?php include 'php/projectsFooter.php'; ?>
+    </div>
+
+    <!--closing javascript -->
+    <?php include 'php/js.php'; ?>
+  </body>
+</html>
---
+++ php/head.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="ico/favicon.png">
+
+    <title>Sajid Reshamwala-Aggregator</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="stylesheets/styles.css" rel="stylesheet" type="text/css">
+
+    <!-- animate.css for simple animations http://daneden.github.io/animate.css/ -->
+    <link rel="stylesheet" href="stylesheets/animate.css" type="text/css">
+
+    <!--Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Kite+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=EB+Garamond&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
+
+    <!-- wow.js -->
+    <script src="js/wow.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+</html>
